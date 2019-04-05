e06eb20ad98782a75ca309a54d69a21919c5d0cc
Chad Jemmett <chad.jemmett@gmail.com>
2019-04-05 16:50:34 -0600

    ex1 is almost working

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,66 +8,30 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 
   HashTable *ht = create_hash_table(16);
   for(int i = 0; i< length; i ++ ) {
-    printf("running loop\n");
-    unsigned int index = hash(weights[i], length);
-    /* printf("the value at index: %d\n", ht->storage[index]); */
-    if(ht->storage[index] == '\0') {
-      printf("checking for 0\n");
+    if(ht->storage[i] == '\0') {
       hash_table_insert(ht, weights[i], i);
-      /* LinkedPair *new_pair = create_pair(weights[i], i); */
-      /* ht->storage[index] = new_pair; */
-      printf("Adding new pair: %d\n", ht->storage[index]->value);
-      /* printf("checking for null: %d\n", ht->storage[index]); */
     }
 
-    /* if(ht->storage[index] == '\0') { */
-      /* LinkedPair *new_pair = create_pair(weights[i], i); */
-    /*   printf("new node", new_pair->value); */
-    /*   printf("checking for 0\n"); */
-    /*   /1* ht->storage[index] = new_pair; *1/ */
-    /* } else { */
-    /*   printf("found a LL\n"); */
+    }
 
-    /*   LinkedPair *thing = ht->storage[index]; */
+    for(int i = 0; i<length; i++) {
+      int difference = limit - weights[i];
+      int result = hash_table_retrieve(ht, difference);
+      printf("hash from result: %d\n", hash_table_retrieve(ht, result));
 
-    /*   printf("Made a node \n", thing->value); */
+      if(result != -1) {
+        Answer * ans = malloc(sizeof(Answer));
+        ans->index_1 = result;
+        ans->index_2 = i;
+      return ans;
+      }
+        /* printf("not the one: %d\n", result); */
 
-      /* while(node->next != NULL) { */
-      /*   printf("looking for end of LL\n"); */
-      /*   node = node->next; */
-      /* } */
-      /* LinkedPair *n = create_pair(weights[i], i); */
-      /* node->next = n; */
-    /* } */
-  }
+      /* printf("index: %d, diff: %d\n", hash_table_retrieve(ht, difference), difference); */
+    }
 
-  /* printf("weights %d, length: %d limit: %d\n", weights, length, limit); */
-  /* /1* printf("hash table data: %d\n", ht->storage[7]); *1/ */
-  /* for(int i = 0; i < length ; i ++) { */
-  /*   unsigned int index = hash(weights[i], length); */
-  /*   if(ht->storage[index] == '\0') { */
-  /*     LinkedPair *new_pair = create_pair(weights[i], i); */
-  /*     /1* printf("found a zero\n"); *1/ */
-  /*     ht->storage[index] = new_pair; */
-  /*     /1* printf("key: %d value: %d", ht->storage[index]->key, ht->storage[index]->value); *1/ */
-  /*   } else { */
-  /*     int diff = limit - weights[i]; */
-  /*     /1* printf("diff %d\n", diff); *1/ */
-  /*     printf("the value at diff %d\n", ht->storage[diff]->value); */
-      /* printf("have to check to see if threere's a LL \n"); */
+    return NULL;
 
-
-
-    /* } */
-    /* printf("index: %d\n", index); */
-    /* printf("hash table data: %d\n", ht->storage[index]); */
-  /* } */
-  // YOUR CODE HER\nE
-  /* for(int i = 0; i < length; i++) { */
-  /*   printf("value: %d", ht->storage[i]->value); */
-  /* } */
-
-  return NULL;
 }
 
 void print_answer(Answer *answer)
@@ -88,17 +52,17 @@ int main(void)
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
   print_answer(answer_1);  // NULL
 
-  // TEST 2
+  /* /1* // TEST 2 *1/ */
   int weights_2[] = {4, 4};
   Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
   print_answer(answer_2);  // {1, 0}
 
-  // TEST 3
+  /* // TEST 3 */
   int weights_3[] = {4, 6, 10, 15, 16};
   Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
   print_answer(answer_3);  // {3, 1}
 
-  // TEST 4
+  /* // TEST 4 */
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
   Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
   print_answer(answer_4);  // {6, 2}
